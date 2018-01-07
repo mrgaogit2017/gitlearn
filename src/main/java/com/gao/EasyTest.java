a42fa767c57cb532362e44e259c0cd969269bcff
mrgaogit2017 <836505824>
2018-01-08 01:27:39 +0800

    1

--- src/main/java/com/gao/EasyTest.java
+++ src/main/java/com/gao/EasyTest.java
@@ -3,6 +3,6 @@ package com.gao;
 public class EasyTest {
     public static void main(String[] args) {
         System.out.println("1e33320");
-        System.out.println(9989);
+        System.out.println(99819);
     }
 }
