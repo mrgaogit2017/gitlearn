22cebc85e0348930ab969bb12cc1a873d2bf64d5
mrgaogit2017 <30492839+mrgaogit2017@users.noreply.github.com>
2018-01-08 00:54:07 +0800

    Update EasyTest.java
    666

--- src/main/java/com/gao/EasyTest.java
+++ src/main/java/com/gao/EasyTest.java
@@ -3,6 +3,6 @@ package com.gao;
 public class EasyTest {
     public static void main(String[] args) {
         System.out.println("16669020");
-        System.out.println(99999);
+        System.out.println(999966669);
     }
 }
