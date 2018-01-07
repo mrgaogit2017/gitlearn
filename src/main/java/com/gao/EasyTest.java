c607c0b1068ae0d15cbb1c5691d3ee09056fd57b
mrgaogit2017 <836505824>
2018-01-07 23:55:11 +0800

    git使用学习

--- src/main/java/com/gao/EasyTest.java
+++ src/main/java/com/gao/EasyTest.java
@@ -2,6 +2,6 @@ package com.gao;
 
 public class EasyTest {
     public static void main(String[] args) {
-        System.out.println("111110020");
+        System.out.println("1111109999020");
     }
 }
