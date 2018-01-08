bd62cb5476f12acbb834e8e7245a687e2f4d0fd6
gaozhaolu <gaozhaolu@jinhuhang.com.cn>
2018-01-08 09:27:24 +0800

    添加A1类

---
+++ src/main/java/com/gao/A1.java
@@ -0,0 +1,13 @@
+package com.gao;
+
+public class A1 {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
