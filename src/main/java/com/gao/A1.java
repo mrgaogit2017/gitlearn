313cb4a5f7462fc28e812ce3ec047266c031a101
gaozhaolu <gaozhaolu@jinhuhang.com.cn>
2018-03-05 09:15:39 +0800

    2018-03-05测试tags

--- src/main/java/com/gao/A1.java
+++ src/main/java/com/gao/A1.java
@@ -3,11 +3,21 @@ package com.gao;
 public class A1 {
     private String name;
 
+    private Integer age;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name+"98";
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
