6fd7b056f4d8d3a0d29625f417d3d1f5d688ced7
gaozhaolu <gaozhaolu@jinhuhang.com.cn>
2018-03-13 09:57:58 +0800

    添加core核心类

---
+++ src/main/java/com/gao/service/core.java
@@ -0,0 +1,17 @@
+package com.gao.service;
+
+/**
+ * 核心层
+ */
+public class core {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public String eatAndRun() {
+        return "123";
+    }
+
+
+}
